5a59a457f2392dd71829a2abeb0e48b383d3afba
PutraAlrasy03 <2019481766@isiswa.uitm.edu.my>
2022-12-14 14:34:38 +0800

    Update

---
+++ Test Suites/Bonus/BDD_Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BDD_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7c1d09d3-c32e-4702-a9dc-7061436ae1d4</testSuiteGuid>
+   <testCaseLink>
+      <guid>810d7afe-f3fc-40ff-9eab-b4205be1f45d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bonus/BDD _Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
